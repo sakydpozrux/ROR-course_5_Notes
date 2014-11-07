353d81325ecbb58ea16b5284b43aedd380acd85c
Szymon Koper <sakydpozrux@gmail.com>
2014-11-07 01:16:44 +0100

    Migration with Note model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141106235024) do
+ActiveRecord::Schema.define(version: 20141107001445) do
+
+  create_table "notes", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "notes", ["user_id"], name: "index_notes_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
